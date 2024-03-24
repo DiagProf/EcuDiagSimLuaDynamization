7683b90949255842d5a0d51c1aa8b4ecc1365dd6
DiagProf <vmax28575@gmail.com>
2024-03-24 18:04:47 +0100

    2 edge cases added

--- RandomName_7A6_7A7.lua
+++ RandomName_7A6_7A7.lua
@@ -4,8 +4,13 @@ YourEcuName = {
 	Raw = {
 		["10 03"] = "50 03 00 28 00 C8",
 		["3E 00"] = "7E 00",
+		["22 08 15"] = "62 08 15 13", -- will not be changed because there is no writing counterpart to DID 0815
 		["22 22 35"] = "62 22 35 47 11",
 		["2E 22 35 47 11"] = "6E 22 35",
+		["22 F1 99"] = "62 F1 99 20 21 11 11",
+		["2E F1 99 20 21 11 11"] = "6E F1 99",
+		["22 F1 10"] = "62 F1 10 44 77 11 00", -- will not be changed because DID F110 does not have the same reading and writing length.
+		["2E F1 10 44 77"] = "6E F1 10", -- will not be changed because DID F110 does not have the same reading and writing length.
         -- Additional entries here
     }
 }
--- RandomName_7A6_7A7_AfterRework.lua
+++ RandomName_7A6_7A7_AfterRework.lua
@@ -3,12 +3,19 @@ YourEcuName = {
 	ResponseId = 0x7A7,
 	DIDs = {
 		["22 35"] = "47 11",
+		["F1 99"] = "20 21 11 11",
 	},
 	Raw = {
 		["10 03"] = "50 03 00 28 00 C8",
 		["3E 00"] = "7E 00",
+		["22 08 15"] = "62 08 15 13", -- will not be changed because there is no writing counterpart to DID 0815
 		["22 22 35"] =  function(request) return getDidData(YourEcuName,request) end,
 		["2E 22 35 *"] = function(request) return updateDidData(YourEcuName,request ) end,
+		["22 F1 99"] =  function(request) return getDidData(YourEcuName,request) end,
+		["2E F1 99 *"] = function(request) return updateDidData(YourEcuName,request ) end,
+		["22 F1 10"] = "62 F1 10 44 77 11 00", -- will not be changed because DID F110 does not have the same reading and writing length.
+		["2E F1 10 44 77"] = "6E F1 10", -- will not be changed because DID F110 does not have the same reading and writing length.
+
         -- Additional entries here
     }
 }
